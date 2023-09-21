f021890acfcb4f1578505650589d1ba627a548ad
Erkz <Erkiparna7@gmail.com>
2023-09-21 15:32:13 +0300

    Ülesanne 5 - vormi kuvamine ja asendamine nuppuga

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import "./ExpenseForm.css"
 
-const ExpenseForm = (props) =>{
+const ExpenseForm = (props) => {
+    const [formShow, setFormShow] = useState(false)
+    const [buttonShow, setButtonShow] = useState(true)
     const [enteredTitle,setEnteredTitle] = useState('')
     const [enteredAmount,setEnteredAmount] = useState('')
     const [enteredDate,setEnteredDate] = useState('')
@@ -26,27 +28,60 @@ const ExpenseForm = (props) =>{
         setEnteredAmount('')
         setEnteredDate('')
     }
-    return(
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
 
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler} value={enteredDate}/>
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
+    const showForm = () =>{
+        if (formShow === true) {
+            setFormShow(false)
+            setEnteredTitle('')
+            setEnteredAmount('')
+            setEnteredDate('')
+            showButton()
+        } else if (formShow === false) {
+            setFormShow(true)
+            setEnteredTitle('')
+            setEnteredAmount('')
+            setEnteredDate('')
+            showButton()
+        }
+    }
+    const showButton = () =>{
+        setButtonShow(!buttonShow)
+    }
+
+    return(
+        <div>
+            {buttonShow ?(
+                <button  onClick={showForm}>Add New Expense</button>
+            ):null
+            }
+            { formShow ?(
+                <form onSubmit={submitHandler}>
+                    <div className="new-expense__controls">
+                        <div className="new-expense__control">
+                            <label>Title</label>
+                            <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
+
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Amount</label>
+                            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Date</label>
+                            <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler} value={enteredDate}/>
+                        </div>
+                    </div>
+                    <div className="new-expense__actions" style={{display:"inline-block"}}>
+                        <button type="submit">Add Expense</button>
+                    </div>
+                    <div className="new-expense__actions" style={{display:"inline-block"}}>
+                        <button onClick={showForm}>Cancel</button>
+                    </div>
+                </form>
+
+            ):null
+            }
+        </div>
     )
 }
 
