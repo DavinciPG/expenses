a16442f71329818fee07c3a4f5f50d01d3ec2688
Erkz <Erkiparna7@gmail.com>
2023-09-21 15:29:16 +0300

    Ülesanne 4 - aasta järgi filtreeritud andmete kuvamine

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 
 import React, { useState } from "react";
+import { flushSync } from 'react-dom';
 
 const DUMMY_EXPENSES = [
     {
         id:"e1",
-        date: new Date(2023, 0, 10),
+        date: new Date(2024, 0, 10),
         title: 'New book',
         price: 30.99
     },
     {
         id:"e2",
-        date: new Date(2023, 0, 10),
+        date: new Date(2024, 0, 10),
         title: 'New Jeans',
         price: 99.99
     }
@@ -33,6 +34,10 @@ const App = () => {
 
     const filterExpenseHandler = (filter) =>{
         console.log('Filter Data in App.js', filter)
+        const filteredExpenses = DUMMY_EXPENSES.filter(
+            (expense) => expense.date.getFullYear() == filter
+        );
+        setExpenses([...filteredExpenses])
     }
 
   return (
